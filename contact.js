e73593557dc1a96998770d7f2f97fc579f8b5eae
KhaDoan209 <doanvinhkha@gmail.com>
2022-07-07 00:40:25 +0700

    contact_done

--- contact.js
+++ contact.js
@@ -1,19 +1,109 @@
-function validateForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+function validateFormContact() {
+    let correct = true;
+    var name = nameCheck();
+    var email = emailCheck();
+    var subject = subjectCheck();
+    var mess = messageCheck();
 
-    if(name == ""){
-        alert("Please enter your name");
-        return false
-    }
-    else if (email = "")
-    {
-        alert("Please enter your email");
-        return false
-    }
-    else {
-        alert("nice");
+    if ((name != correct) || (email != correct) || (subject != correct) || (mess != correct)) {
+
+        return false;
+
+    } else {
+        alert("Message sent successfully");
         return true;
     }
-    
-  }
+
+}
+
+function nameCheck() {
+    var name = document.getElementById("name").value;
+    var error = document.getElementById("error-name");
+
+    if (name == "") {
+        error.textContent = "Please fill out your name.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else if (name.length > 100) {
+        error.textContent = "Your name should not exceed 100 characters.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else
+
+        error.textContent = "";
+
+
+    return true;
+}
+
+function emailCheck() {
+    var email = document.getElementById("email").value;
+    var emailRegular = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var error = document.getElementById("error-email");
+
+    if (email == "") {
+        error.textContent = "Please fill out your email.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else if (email.length > 100) {
+        error.textContent = "Your email should not exceed 100 characters.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else if (emailRegular.test(email) == false) {
+        error.textContent = "Please enter a valid email.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+    } else
+        error.textContent = "";
+
+    return true
+}
+
+function subjectCheck() {
+    var subject = document.getElementById("subject").value;
+    var error = document.getElementById("error-subject");
+
+    if (subject == "") {
+        error.textContent = "Please fill out the subject.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else if ((subject.length < 50) && (subject.length > 1)) {
+        error.textContent = "Subject should not shorter than 50 characters.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else if (subject.length > 250) {
+        error.textContent = "Subject should not longer than 250 characters.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+    } else
+        error.textContent = "";
+
+    return true
+
+}
+
+function messageCheck() {
+    var mess = document.getElementById("message").value;
+    var error = document.getElementById("error-mess");
+
+    if (mess == "") {
+        error.textContent = "Please fill out the message.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+
+    } else if (mess.length > 500) {
+        error.textContent = "Message should not exceed 500 characters.";
+        error.style.backgroundColor = "#D0342C";
+        return false;
+    } else
+        error.textContent = "";
+
+    return true
+
+}
--- order.js
+++ order.js
@@ -1,107 +1,94 @@
-function validateFormContact() {
+function validateFormOrder() {
     let correct = true;
-    var name = nameCheck();
-    var email = emailCheck();
-    var subject = subjectCheck();
-    var mess = messageCheck();
 
-    if ((name != correct) || (email != correct) || (subject != correct) || (mess != correct)) {
+    var cake = cakeSelectCheck();
+    var name = nameCheck();
+    var mess = messageCheck();
+    var deliDate = deliverDateCheck();
+    var deliAddress = deliverAddressCheck();
+
+    if ((cake != correct) || (name != correct) || (mess != correct) || (deliDate != correct) || (deliAddress != correct)) {
 
         return false;
+
     } else {
 
-        alert("Message sent successfully");
-        console.log("hehe");
+        alert("Oder has been processed successfully");
         return true;
     }
+}
 
+function cakeSelectCheck() {
+    var cake = document.getElementById("cakeList");
+    var error = document.getElementById("error-cake");
+    if (cake.value == "--- Select cake ---") {
+        error.textContent = "Please select our product.";
+        error.style.color = "white";
+
+        return false;
+    } else
+        error.textContent = "";
+    return true;
 }
 
 function nameCheck() {
-    var name = document.getElementById("name").value;
+    var name = document.getElementById("name");
     var error = document.getElementById("error-name");
 
-    if (name == "") {
+    if (name.value.length == "") {
         error.textContent = "Please fill out your name.";
         error.style.color = "red";
+        errorStyle(error);
         return false;
-
-    } else if (name.length > 100) {
+    } else if (name.value.length > 100) {
         error.textContent = "Your name should not exceed 100 characters.";
         error.style.color = "red";
         return false;
-
     } else
-
         error.textContent = "";
+    return true;
+}
 
+function messageCheck() {
+    var mess = document.getElementById("message");
+    var error = document.getElementById("error-message");
+
+    if (mess.value.length > 30) {
+        error.textContent = "Message should not exceed 30 characters";
+        error.style.color = "red";
+        return false;
+    } else error.textContent = "";
 
     return true;
 }
 
-function emailCheck() {
-    var email = document.getElementById("email").value;
-    var emailRegular = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var error = document.getElementById("error-email");
+function deliverDateCheck() {
+    var deliverDate = document.getElementById("delivery-date");
+    var error = document.getElementById("error-deli-date");
 
-    if (email == "") {
-        error.textContent = "Please fill out your email.";
+
+    if (deliverDate.value == "") {
+        error.textContent = "Please choose a delivery date";
         error.style.color = "red";
         return false;
+    } else error.textContent = "";
 
-    } else if (email.length > 100) {
-        error.textContent = "Your email should not exceed 100 characters.";
-        error.style.color = "red";
-        return false;
-
-    } else if (emailRegular.test(email) == false) {
-        error.textContent = "Please enter a valid email.";
-        error.style.color = "red";
-        return false;
-    } else
-        error.textContent = "";
-    return true
+    return true;
 }
 
-function subjectCheck() {
-    var subject = document.getElementById("subject").value;
-    var error = document.getElementById("error-subject");
+function deliverAddressCheck() {
+    var deliverAddress = document.getElementById("delivery-address");
+    var error = document.getElementById("error-deli-address");
 
-    if (subject == "") {
-        error.textContent = "Please fill out the subject.";
+    if (deliverAddress.value == "") {
+        error.textContent = "Please fill out the address.";
         error.style.color = "red";
         return false;
-
-    } else if ((subject.length < 50) && (subject.length > 1)) {
-        error.textContent = "Subject should not shorter than 50 characters.";
+    } else if (deliverAddress.value.length > 500) {
+        error.textContent = "Address should not exceed 500 characters.";
         error.style.color = "red";
         return false;
+    } else error.textContent = "";
 
-    } else if (subject.length > 250) {
-        error.textContent = "Subject should not longer than 250 characters.";
-        error.style.color = "red";
-        return false;
-    } else
-        error.textContent = "";
-    return true
-
-}
-
-function messageCheck() {
-    var mess = document.getElementById("message").value;
-    var error = document.getElementById("error-mess");
-
-    if (mess == "") {
-        error.textContent = "Please fill out the message.";
-        error.style.color = "red";
-        return false;
-
-    } else if (mess.length > 500) {
-        error.textContent = "Message should not exceed 500 characters.";
-        error.style.color = "red";
-        return false;
-    } else
-        error.textContent = "";
-    return true
-
+    return true;
 }
